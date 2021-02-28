da9d0b85946e127ac21b3cabcfd9219a38c7b8d1
Cristiano Viana <cristiano.gvianati@gmail.com>
2021-02-28 12:29:58 -0300

    CORS

--- src/main/java/io/github/viana2015/csp/rest/UsuarioController.java
+++ src/main/java/io/github/viana2015/csp/rest/UsuarioController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import io.github.viana2015.csp.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@CrossOrigin("*")
 public class UsuarioController {
 
 	@Autowired
